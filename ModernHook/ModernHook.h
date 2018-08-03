480869920b07ce366e6b613d4d6a59caf0d2f7a4
John Smith <xfgryujk@126.com>
2018-08-03 10:06:48 +0800

    Support calling conventions

--- ModernHook/ModernHook.h
+++ ModernHook/ModernHook.h
@@ -7,6 +7,47 @@
 namespace ModernHook
 {
 
+// Support calling conventions
+
+#define DEF_CDECL(FUNC, OPT1, OPT2, OPT3) \
+	FUNC(__cdecl, OPT1, OPT2, OPT3)
+#ifdef _M_CEE
+#define DEF_CLRCALL(FUNC, OPT1, OPT2, OPT3) \
+	FUNC(__clrcall, OPT1, OPT2, OPT3)
+#else
+#define DEF_CLRCALL(FUNC, OPT1, OPT2, OPT3)
+#endif
+#if defined(_M_IX86) && !defined(_M_CEE)
+#define DEF_FASTCALL(FUNC, OPT1, OPT2, OPT3) \
+	FUNC(__fastcall, OPT1, OPT2, OPT3)
+#else
+#define DEF_FASTCALL(FUNC, OPT1, OPT2, OPT3)
+#endif
+#ifdef _M_IX86
+#define DEF_STDCALL(FUNC, OPT1, OPT2, OPT3) \
+	FUNC(__stdcall, OPT1, OPT2, OPT3)
+#define DEF_THISCALL(FUNC, OPT1, OPT2, OPT3) \
+	FUNC(__thiscall, OPT1, OPT2, OPT3)
+#else
+#define DEF_STDCALL(FUNC, OPT1, OPT2, OPT3)
+#define DEF_THISCALL(FUNC, OPT1, OPT2, OPT3)
+#endif
+#if ((defined(_M_IX86) && _M_IX86_FP >= 2) \
+	|| defined(_M_X64)) && !defined(_M_CEE)
+#define DEF_VECTORCALL(FUNC, OPT1, OPT2, OPT3) \
+	FUNC(__vectorcall, OPT1, OPT2, OPT3)
+#else
+#define DEF_VECTORCALL(FUNC, OPT1, OPT2, OPT3)
+#endif
+
+#define DEF_NON_MEMBER(FUNC, CV_OPT, REF_OPT, NOEXCEPT_OPT) \
+	DEF_CDECL(FUNC, CV_OPT, REF_OPT, NOEXCEPT_OPT) \
+	DEF_CLRCALL(FUNC, CV_OPT, REF_OPT, NOEXCEPT_OPT) \
+	DEF_FASTCALL(FUNC, CV_OPT, REF_OPT, NOEXCEPT_OPT) \
+	DEF_STDCALL(FUNC, CV_OPT, REF_OPT, NOEXCEPT_OPT) \
+	DEF_VECTORCALL(FUNC, CV_OPT, REF_OPT, NOEXCEPT_OPT)
+
+
 class BaseHook
 {
 protected:
@@ -23,22 +64,23 @@ protected:
 	virtual void DoDisable() = 0;
 };
 
-// TODO Support other calling conventions
-#define CV __stdcall
-
 template<class FunctionType>
 class Hook;
 
-template<class ResultType, class... ArgTypes>
-class Hook<ResultType CV(ArgTypes...)> : public BaseHook
-{
-public:
-	using FunctionType = ResultType CV(ArgTypes...);
+#define DEF_HOOK(CV, X1, X2, X3) \
+	template<class ResultType, class... ArgTypes>						\
+	class Hook<ResultType CV(ArgTypes...)> : public BaseHook			\
+	{																	\
+	public:																\
+		using FunctionType = ResultType CV(ArgTypes...);				\
+																		\
+	public:																\
+		virtual ~Hook() = default;										\
+		virtual ResultType CallOriginalFunction(ArgTypes... args) = 0;	\
+	};
+DEF_NON_MEMBER(DEF_HOOK, X1, X2, X3)
+#undef DEF_HOOK
 
-public:
-	virtual ~Hook() = default;
-	virtual ResultType CallOriginalFunction(ArgTypes... args) = 0;
-};
 
 #pragma pack(push)
 #pragma pack(1)
@@ -65,53 +107,68 @@ public:
 template<class FunctionType>
 class InlineHook;
 
-template<class ResultType, class... ArgTypes>
-class InlineHook<ResultType CV(ArgTypes...)> : public Hook<ResultType CV(ArgTypes...)>
-{
-private:
-	FunctionType* const originalFunction = nullptr;
-	FunctionType* const hookFunction = nullptr;
-	const std::array<uint8_t, sizeof(JmpCode)> originalCode;
+#define DEF_INLINE_HOOK(CV, X1, X2, X3) \
+	template<class ResultType, class... ArgTypes>														\
+	class InlineHook<ResultType CV(ArgTypes...)> : public Hook<ResultType CV(ArgTypes...)>				\
+	{																									\
+	private:																							\
+		FunctionType* const originalFunction = nullptr;													\
+		FunctionType* const hookFunction = nullptr;														\
+		const std::array<uint8_t, sizeof(JmpCode)> originalCode;										\
+																										\
+	public:																								\
+		InlineHook(FunctionType* _originalFunction, FunctionType* _hookFunction, bool enable = true) :	\
+			originalFunction(_originalFunction),														\
+			hookFunction(_hookFunction)																	\
+		{																								\
+			memcpy((void*)&originalCode, originalFunction, sizeof(originalCode));						\
+																										\
+			if (enable)																					\
+				Enable();																				\
+		}																								\
+																										\
+		virtual ~InlineHook() { Disable(); }															\
+																										\
+		virtual void DoEnable()																			\
+		{																								\
+			JmpCode code((uintptr_t)originalFunction, (uintptr_t)hookFunction);							\
+			DWORD oldProtect, oldProtect2;																\
+			VirtualProtect(originalFunction, sizeof(JmpCode), PAGE_EXECUTE_READWRITE, &oldProtect);		\
+			memcpy(originalFunction, &code, sizeof(JmpCode));											\
+			VirtualProtect(originalFunction, sizeof(JmpCode), oldProtect, &oldProtect2);				\
+		}																								\
+																										\
+		virtual void DoDisable()																		\
+		{																								\
+			DWORD oldProtect, oldProtect2;																\
+			VirtualProtect(originalFunction, sizeof(JmpCode), PAGE_EXECUTE_READWRITE, &oldProtect);		\
+			memcpy(originalFunction, &originalCode, sizeof(originalCode));								\
+			VirtualProtect(originalFunction, sizeof(JmpCode), oldProtect, &oldProtect2);				\
+		}																								\
+																										\
+		virtual ResultType CallOriginalFunction(ArgTypes... args)										\
+		{																								\
+			bool originalEnable = IsEnabled();															\
+			Disable();																					\
+			ResultType result = originalFunction(args...);												\
+			if (originalEnable)																			\
+				Enable();																				\
+			return result;																				\
+		}																								\
+	};
+DEF_NON_MEMBER(DEF_INLINE_HOOK, X1, X2, X3)
+#undef DEF_INLINE_HOOK
 
-public:
-	InlineHook(FunctionType* _originalFunction, FunctionType* _hookFunction, bool enable = true) :
-		originalFunction(_originalFunction),
-		hookFunction(_hookFunction)
-	{
-		memcpy((void*)&originalCode, originalFunction, sizeof(originalCode));
+// Clear defines
+#ifndef MODERN_HOOK_DONT_CLEAR_DEFINES
+#undef DEF_CDECL
+#undef DEF_CLRCALL
+#undef DEF_FASTCALL
+#undef DEF_STDCALL
+#undef DEF_THISCALL
+#undef DEF_VECTORCALL
 
-		if (enable)
-			Enable();
-	}
-
-	virtual ~InlineHook() { Disable(); }
-
-	virtual void DoEnable()
-	{
-		JmpCode code((uintptr_t)originalFunction, (uintptr_t)hookFunction);
-		DWORD oldProtect, oldProtect2;
-		VirtualProtect(originalFunction, sizeof(JmpCode), PAGE_EXECUTE_READWRITE, &oldProtect);
-		memcpy(originalFunction, &code, sizeof(JmpCode));
-		VirtualProtect(originalFunction, sizeof(JmpCode), oldProtect, &oldProtect2);
-	}
-
-	virtual void DoDisable()
-	{
-		DWORD oldProtect, oldProtect2;
-		VirtualProtect(originalFunction, sizeof(JmpCode), PAGE_EXECUTE_READWRITE, &oldProtect);
-		memcpy(originalFunction, &originalCode, sizeof(originalCode));
-		VirtualProtect(originalFunction, sizeof(JmpCode), oldProtect, &oldProtect2);
-	}
-
-	virtual ResultType CallOriginalFunction(ArgTypes... args)
-	{
-		bool originalEnable = IsEnabled();
-		Disable();
-		ResultType result = originalFunction(args...);
-		if (originalEnable)
-			Enable();
-		return result;
-	}
-};
+#undef DEF_NON_MEMBER
+#endif
 
 }
--- Test/unittest1.cpp
+++ Test/unittest1.cpp
@@ -1,42 +1,49 @@
 ﻿#include "stdafx.h"
 #include "CppUnitTest.h"
 
-#include <string>
-
+#define MODERN_HOOK_DONT_CLEAR_DEFINES
 #include "ModernHook.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-using namespace std;
 
 using namespace ModernHook;
 
 namespace Test
 {
-	int __stdcall Add(int a, int b)
-	{
-		return a + b;
+
+#define DEF_ADD(CV, X1, X2, X3) \
+	int CV Add##CV(int a, int b)		\
+	{									\
+		return a + b;					\
 	}
+DEF_NON_MEMBER(DEF_ADD, X1, X2, X3)
 
-	TEST_CLASS(TestInlineHook)
-	{
-	private:
-		static int __stdcall MyAdd(int a, int b)
-		{
-			return a - b;
+TEST_CLASS(TestInlineHook)
+{
+private:
+	#define DEF_MY_ADD(CV, X1, X2, X3) \
+		static int CV MyAdd##CV(int a, int b)		\
+		{											\
+			return a - b;							\
 		}
+	DEF_NON_MEMBER(DEF_MY_ADD, X1, X2, X3)
 
-	public:
-		TEST_METHOD(Hook)
-		{
-			InlineHook<decltype(Add)> messageBoxWHook(Add, MyAdd);
-			Assert::AreEqual(0, Add(1, 1));
+public:
+	#define DEF_TEST_HOOK(CV, X1, X2, X3) \
+		TEST_METHOD(Hook##CV)																\
+		{																					\
+			InlineHook<decltype(Add##CV)> messageBoxWHook(Add##CV, MyAdd##CV);				\
+			Assert::AreEqual(0, Add##CV(1, 1));												\
 		}
+	DEF_NON_MEMBER(DEF_TEST_HOOK, X1, X2, X3)
 
-		TEST_METHOD(CallOriginalFunction)
-		{
-			InlineHook<decltype(Add)> messageBoxWHook(Add, MyAdd);
-			Assert::AreEqual(2, messageBoxWHook.CallOriginalFunction(1, 1));
+	#define DEF_TEST_CALL_ORIG(CV, X1, X2, X3) \
+		TEST_METHOD(CallOriginalFunction##CV)												\
+		{																					\
+			InlineHook<decltype(Add##CV)> messageBoxWHook(Add##CV, MyAdd##CV);				\
+			Assert::AreEqual(2, messageBoxWHook.CallOriginalFunction(1, 1));				\
 		}
-	};
+	DEF_NON_MEMBER(DEF_TEST_CALL_ORIG, X1, X2, X3)
+};
+
 }
